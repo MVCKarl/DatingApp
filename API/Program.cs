ee9046931f16212ac68eb298d2a1db97c6d829d0
Karl Thomas <karlthomas23@gmail.com>
2023-06-24 20:30:54 +0930

    End of Section 3. Added https

--- API/Program.cs
+++ API/Program.cs
@@ -12,10 +12,12 @@ builder.Services.AddDbContext<DataContext>(option =>
 });
 builder.Services.AddCors();
 
+builder.WebHost.UseKestrel(option => option.AddServerHeader = false);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.MapControllers();
 
 app.Run();
